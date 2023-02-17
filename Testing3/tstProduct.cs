e208ce532a4c2233b4957ea566a0542e29812abc
Marco Escaleira <marcoescaleira2000@gmail.com>
2023-02-17 00:39:04 +0000

    github day marco - class Products and test

---
+++ Testing3/tstProduct.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingProduct
+{
+    [TestClass]
+    public class tstProduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsProduct anProduct = new clsProduct();
+
+            Assert.IsNotNull(anProduct);
+        }
+    }
+}
