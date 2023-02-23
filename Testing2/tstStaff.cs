aa3ba6ec522db98c2907d6e61aa6f04eebf35875
Alex <p2665879@dmu.ac.uk>
2023-02-23 12:08:42 +0000

    GitHub Work Day document done !

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test to see if it exists
+            Assert.IsNotNull(AStaff);
+        }
+        public void TestMethod1()
+        {
+        }
+    }
+}
