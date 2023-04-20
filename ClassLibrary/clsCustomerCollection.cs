f46cc282ca2c1aabc9c9e43ca8494cbee46139dd
Adrian <p2662151@my365.dmu.ac.uk>
2023-04-20 14:47:30 +0000

    start collection

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        //private data member for the list
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                //return the private data
+                return mCustomerList;
+            }
+
+            set
+            {
+                //set the private data
+                mCustomerList = value;
+            }
+        }
+        public int Count 
+        {
+            get
+            {
+                //return the count of the list
+                return mCustomerList.Count;
+            }
+
+            set
+            {
+                
+            }
+        }
+        public clsCustomer ThisCustomer { get; set; }
+
+
+        public clsCustomerCollection()
+        {
+    
+
+            //crate the items of the test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its proprieties
+            TestItem.CustomerConfirmed = true;
+            TestItem.CustomerNo = 1001;
+            TestItem.CustomerAddress = "Not here, NG8 5LH";
+            TestItem.CustomerEmail = "daniel.hatel@yahoo.com";
+            TestItem.CustomerName = "Daniel Hatel";
+            TestItem.CustomerDateJoined = DateTime.Now.Date;
+            //add the item to the test list
+            mCustomerList.Add(TestItem);
+            //re initialise the object for some new data
+            TestItem = new clsCustomer();
+            //set it's properties
+            TestItem.CustomerConfirmed = false;
+            TestItem.CustomerNo = 1002;
+            TestItem.CustomerAddress = "This Street, LE 3 GB2";
+            TestItem.CustomerEmail = "anil.note@yahoo.com";
+            TestItem.CustomerName = "Anil Note";
+            TestItem.CustomerDateJoined = DateTime.Now.Date;
+
+        }
+        public clsCustomerCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stired oricedure
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //get the count of record
+            RecordCount = DB.Count;
+            //while there are record to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsCustomer AnCustomer = new clsCustomer();
+                //read in the fields from the current record
+                AnCustomer.CustomerConfirmed = Convert.ToBoolean(DB.DataTable.Rows[Index]["CustomerConfirmed"]);
+                AnCustomer.CustomerNo = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerNo"]);
+                AnCustomer.CustomerAddress = Convert.ToString(DB.DataTable.Rows[Index]["CustomerAddress"]);
+                AnCustomer.CustomerEmail = Convert.ToString(DB.DataTable.Rows[Index]["CustomerEmail"]);
+                AnCustomer.CustomerName = Convert.ToString(DB.DataTable.Rows[Index]["CustomerName"]);
+                AnCustomer.CustomerDateJoined = Convert.ToDateTime(DB.DataTable.Rows[Index]["CustomerDateJoined"]);
+                //add the record to the private data member
+                mCustomerList.Add(AnCustomer);
+                //point at the next record
+                Index++;
+
+            }
+        }
+    }
+
+
+}
---
+++ Testing1/tstCustomerCollection.cs
@@ -0,0 +1,123 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //test to see that it exist
+            Assert.IsNotNull(AllCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //add an item to the list
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustomerConfirmed = true;
+            TestItem.CustomerNo = 1001;
+            TestItem.CustomerAddress = "Not here, NG8 5LH";
+            TestItem.CustomerEmail = "daniel.hatel@yahoo.com";
+            TestItem.CustomerName = "Daniel Hatel";
+            TestItem.CustomerDateJoined = DateTime.Now.Date;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCustomer.CustomerList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomer.CustomerList, TestList);
+
+        }
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            //create an insyance of the class we want to create
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 2;
+            //assign the data to the property
+            AllCustomer.Count = SomeCount;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomer.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisCustomerPropertOK()
+        {
+            //create an insyance of the class we want to create
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //create some test data to assign to the property
+            clsCustomer TestCustomer = new clsCustomer();
+            //set the property of the test object
+            TestCustomer.CustomerConfirmed = true;
+            TestCustomer.CustomerNo = 1001;
+            TestCustomer.CustomerAddress = "Not here, NG8 5LH";
+            TestCustomer.CustomerEmail = "daniel.hatel@yahoo.com";
+            TestCustomer.CustomerName = "Daniel Hatel";
+            TestCustomer.CustomerDateJoined = DateTime.Now.Date;
+            //asign the data to the property
+            AllCustomer.ThisCustomer = TestCustomer;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomer.ThisCustomer, TestCustomer);
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+            //create an instance of the class we want to create
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            //add an item to the list
+            //create the item of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustomerConfirmed = true;
+            TestItem.CustomerNo = 1001;
+            TestItem.CustomerAddress = "Not here, NG8 5LH";
+            TestItem.CustomerEmail = "daniel.hatel@yahoo.com";
+            TestItem.CustomerName = "Daniel Hatel";
+            TestItem.CustomerDateJoined = DateTime.Now.Date;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCustomer.CustomerList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomer.Count, TestList.Count);
+
+        }
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            //create an instance of the class we want to create
+             clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomer.Count, 2);
+    }
+
+    }
+}
